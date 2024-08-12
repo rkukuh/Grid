ac550a480c1c7ea33195a9a6d3ba9af2387fb7d4
R. Kukuh <rkukuh@gmail.com>
2024-08-12 11:01:59 +0700

    Initial Commit

---
+++ Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by R. Kukuh on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
